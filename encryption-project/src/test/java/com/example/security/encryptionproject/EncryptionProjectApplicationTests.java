3f429dec0723ded30cb5314593e730725342cf92
abrah <abraham.berhe@gmail.com>
2022-10-03 14:48:14 -0400

    Add SB based encryption-project

---
+++ encryption-project/src/test/java/com/example/security/encryptionproject/EncryptionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security.encryptionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
