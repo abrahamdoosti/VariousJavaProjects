071160e5290facea0c470d1aa87181e49ba7bfe7
abrah <abraham.berhe@gmail.com>
2022-10-03 15:00:20 -0400

    Added more Java8 demos

--- JAVA8/src/MethodReference.java
+++ JAVA8/src/MethodReference.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 interface Sayable{
 	void say();
@@ -13,24 +16,25 @@ public class MethodReference {
 		String str1=new String("abebe");
 		System.out.println(str==str1);
 		System.out.println(str.equals(str1));
-		System.out.println( (str.compareTo("bbebe")));
-		System.out.println( (str.compareTo("bekele")));
-		System.out.println( (str.compareTo("a")));
+		System.out.println((str.compareTo("bbebe")));
+		System.out.println((str.compareTo("bekele")));
+		System.out.println((str.compareTo("a")));
 	    // Referring static method
-	    Sayable sayable = new Sayable() {
-
+		Sayable sayable = new Sayable() {
 			@Override
 			public void say() {
 				System.out.println("Hello, this is static method.");
-				
 			}
-	    	
-	    };
-	    Comparator<Product> myComparator=(Product p1, Product p2)->
-	    {
-	    	return p1.name.compareTo(p2.name);
-	    };
-	    
+		};
+		Comparator<Product> myComparator = (Product p1, Product p2) -> p1.name.compareTo(p2.name);
+		
+	    List<Product> products=new ArrayList<>();
+		products.add(new Product(1, "Laptop", 1200f));
+		products.add(new Product(2, "Camera", 500f));
+		products.add(new Product(3, "Phone", 900f));
+		products.add(new Product(4, "TV", 650f));
+	    Collections.sort(products, myComparator);
+	    products.stream().forEach(p->System.out.println(p.toString()));
 	    		//MethodReference::saySomething;
 	    // Calling interface method
 	    sayable.say();
